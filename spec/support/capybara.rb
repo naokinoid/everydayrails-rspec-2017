38096e151ff1bf2845dd38faf82d2f3e5de8fc5f
naokinoid <naoki.itou.0322@gmail.com>
2021-09-30 13:42:49 +0900

    第６章　完了

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,4 +6,7 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
+
+  Capybara.javascript_driver = :selenium_chrome_headless
+
 end
